7459489530c8d78cdd406bdd386947ce504f1485
lunseils <78548868+lunseils@users.noreply.github.com>
2026-04-27 17:47:11 +0800

    Update configure.sh

--- configure.sh
+++ configure.sh
@@ -4,5 +4,12 @@
 # 修改默认IP为192.168.10.1
 sed -i 's/192.168.1.1/192.168.1.111/g' package/base-files/files/bin/config_generate 
 
+# Hello World
+echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
+
+# passwall
+echo "src-git passwall_packages https://github.com/Openwrt-Passwall/openwrt-passwall-packages.git;main" >> feeds.conf.default
+echo "src-git passwall2 https://github.com/Openwrt-Passwall/openwrt-passwall2.git;main" >> feeds.conf.default
+
 # iStore
 echo "src-git istore https://github.com/linkease/istore;main" >> feeds.conf.default
