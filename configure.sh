b3f98b6c4923b0aca25870fc6646c76ff407d67b
lunseils <78548868+lunseils@users.noreply.github.com>
2026-04-27 21:15:55 +0800

    Update configure.sh

--- configure.sh
+++ configure.sh
@@ -4,6 +4,14 @@
 # 修改默认IP为192.168.10.1
 sed -i 's/192.168.1.1/192.168.1.111/g' package/base-files/files/bin/config_generate 
 
+# 自定义默认网关（旁路由必备）
+sed -i '/set network.lan.gateway/d' package/base-files/files/bin/config_generate
+echo "set network.lan.gateway='192.168.1.1'" >> package/base-files/files/bin/config_generate
+
+# 自定义默认 DNS
+sed -i '/set network.lan.dns/d' package/base-files/files/bin/config_generate
+echo "set network.lan.dns='223.5.5.5 114.114.114.114'" >> package/base-files/files/bin/config_generate
+
 # Hello World
 echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 
